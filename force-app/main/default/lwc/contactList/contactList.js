3a32ec3de26380b0e0a9ad1cb3291efdcd42454f
Danylo Hamdzii <danyloulala@gmail.com>
2022-03-02 10:44:36 +0100

    5 challenge

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME from '@salesforce/schema/Contact.LastName';
 import EMAIL from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRST_NAME.fieldApiName, type: 'text' },
     { label: 'Last Name', fieldName: EMAIL.fieldApiName, type: 'text' },
@@ -12,4 +13,8 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
 }
